3708db764f89c3950722632a14b734cc17756fa3
mohamedhassan <mohamed7github@gmail.com>
2024-09-22 06:15:06 +0300

    add quizColor

---
+++ lib/features/quiz/presentation/views/quiz_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/core/utils/app_colors.dart';
+
+class QuizView extends StatelessWidget {
+  const QuizView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: quizaBackgroundColor,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
